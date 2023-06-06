3756a4afbb993468b2ce21be2b01583ddd479654
weiguanghao <1525658286@qq.com>
2023-06-06 16:59:22 +0800

    wgh,feat:初始化工程

---
+++ src/main/java/cn/itedus/mybatis/Main.java
@@ -0,0 +1,12 @@
+package cn.itedus.mybatis;
+
+/**
+ * @author: Guanghao Wei
+ * @date: 2023-06-06 16:54
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
